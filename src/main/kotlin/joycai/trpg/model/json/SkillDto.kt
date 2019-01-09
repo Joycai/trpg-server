262b54b09eff1929104c9a4f24fcd8f248355ea0
zhengxucai <85534078@qq.com>
2019-01-09 16:40:44 +0800

    -update skill model

--- src/main/kotlin/joycai/trpg/model/json/SkillDto.kt
+++ src/main/kotlin/joycai/trpg/model/json/SkillDto.kt
@@ -4,8 +4,8 @@ data class SkillDto(
         var name: String? = null,
         var code: String? = null,
         var base: Int = 0,
-        var value: Int = 0,
-        var value2: Int = 0,
+        var jobPerk: Int = 0,
+        var hobPerk: Int = 0,
         var isJobPerk: Boolean = false,
         var enableJobPerk: Boolean = false,
         var enableHobPerk: Boolean = true
--- src/main/kotlin/joycai/trpg/service/impl/BaseDataServiceImpl.kt
+++ src/main/kotlin/joycai/trpg/service/impl/BaseDataServiceImpl.kt
@@ -32,24 +32,24 @@ class BaseDataServiceImpl(
         //读取手艺
         val artList = readSheet(reader, 0, 1, sheetHeader, SkillDto::class.java)
         if (artList.isNotEmpty()) {
-            artList.forEach { artMap["技艺(${it.name})"] = it }
+            artList.forEach { artMap["${it.code}"] = it }
         }
         //读取科学
         val scList = readSheet(reader, 0, 2, sheetHeader, SkillDto::class.java)
         if (scList.isNotEmpty()) {
-            scList.forEach { scMap["科学(${it.name})"] = it }
+            scList.forEach { scMap["${it.code}"] = it }
         }
 
         //读取驾驶
         val driveList = readSheet(reader, 0, 3, sheetHeader, SkillDto::class.java)
         if (driveList.isNotEmpty()) {
-            driveList.forEach { driveMap["驾驶(${it.name})"] = it }
+            driveList.forEach { driveMap["${it.code}"] = it }
         }
 
         //读取求生
         val surviveList = readSheet(reader, 0, 4, sheetHeader, SkillDto::class.java)
         if (surviveList.isNotEmpty()) {
-            surviveList.forEach { surviveMap["生存(${it.name})"] = it }
+            surviveList.forEach { surviveMap["${it.code}"] = it }
         }
 
         //读取基础技能
@@ -64,20 +64,20 @@ class BaseDataServiceImpl(
                 if (it.code == "108") {
                     it.enableHobPerk = false
                 }
-                baseMap["${it.name}"] = it
+                baseMap["${it.code}"] = it
             }
         }
 
         //读取格斗
         val fightList = readSheet(reader, 0, 6, sheetHeader, SkillDto::class.java)
         if (fightList.isNotEmpty()) {
-            fightList.forEach { fightMap["格斗(${it.name})"] = it }
+            fightList.forEach { fightMap["${it.code}"] = it }
         }
 
         //读取射击
         val shootList = readSheet(reader, 0, 7, sheetHeader, SkillDto::class.java)
         if (shootList.isNotEmpty()) {
-            shootList.forEach { shootMap["射击(${it.name})"] = it }
+            shootList.forEach { shootMap["${it.code}"] = it }
         }
     }
 
@@ -94,12 +94,12 @@ class BaseDataServiceImpl(
     override fun listAllSkill(): Any {
         return mapOf(
                 "base" to baseMap,
-                "sc" to scMap,
-                "sv" to surviveMap,
-                "art" to artMap,
-                "dr" to driveMap,
-                "ft" to fightMap,
-                "st" to shootMap
+                "fight" to fightMap,
+                "shoot" to shootMap,
+                "science" to scMap,
+                "survive" to surviveMap,
+                "craft" to artMap,
+                "drive" to driveMap
         )
     }
 }
